4fff967ee67947def10304fb3c0e3255f77b1162
GuMingYang <1666366054@qq.com>
2020-11-25 10:00:51 +0800

    更改结构体传参

--- PWM/pwm.h
+++ PWM/pwm.h
@@ -2,6 +2,7 @@
 #define _PWM_H
 #include "timer3.h"
 #include "common.h"
+#include "svgen_dq.h"
 #define UP16LIMIT(var, max, min)               \
     {                                          \
         (var) = (var) > (max) ? (max) : (var); \
@@ -12,5 +13,5 @@
 #define Open_Mode 0x01
 #define Close_Mode 0x00
 extern void PortOutput_Config(uint8_t U1, uint8_t U2, uint8_t V1, uint8_t V2, uint8_t W1, uint8_t W2);
-extern void PWMChangeDuty(uint16_t Ta, uint16_t Tb, uint16_t Tc);
+extern void PWMChangeDuty(p_SVGENDQ);
 #endif
