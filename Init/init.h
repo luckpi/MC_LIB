de70e34a2a17f464b4faa868fc4c52974c7ad3c1
GuMingYang <1666366054@qq.com>
2020-11-30 09:41:56 +0800

    删除无用参数

--- Init/init.h
+++ Init/init.h
@@ -6,24 +6,7 @@
 #include "timer3.h"
 #include "flash.h"
 #include "adc.h"
-#define PWMSYSCLK (48000000) // PWM时钟
-#define SYSCLK (48000000)    // 除PWM外的模块时钟
 
-#define PWM_FRE (16000) //PWM的频率
-#define PWM_FRE_SETATA   (1500)                       //自动重装载值((PWMSYSCLK / PWM_FRE) >> 1)
-#define PWM_DUTYCYCLE_95 ((PWM_FRE_SETATA / 20) * 19) // PWM的占空比  95%
-#define PWM_DUTYCYCLE_75 ((PWM_FRE_SETATA / 4) * 3)   // PWM的占空比  75%
-#define PWM_DUTYCYCLE_50 (PWM_FRE_SETATA / 2)         // PWM的占空比  50%
-#define PWM_DUTYCYCLE_25 (PWM_FRE_SETATA / 4)         // PWM的占空比  25%
-#define PWM_DUTYCYCLE_20 (PWM_FRE_SETATA / 5)         // PWM的占空比  20%
-#define PWM_DUTYCYCLE_15 ((PWM_FRE_SETATA / 20) * 3)  // PWM的占空比  15%
-#define PWM_DUTYCYCLE_10 (PWM_FRE_SETATA / 10)        // PWM的占空比  10%
-#define PWM_DUTYCYCLE_05 (PWM_FRE_SETATA / 20)        // PWM的占空比  5%
-#define PWM_DUTYCYCLE_00 (1)                          // PWM的占空比  0%
-
-#define PWM_START_DUTY (PWM_DUTYCYCLE_10)
-#define PWM_MIN_DUTY (PWM_DUTYCYCLE_05) // 最小占空比
-#define PWM_MAX_DUTY PWM_FRE_SETATA     // 最大占空比
 #define OP0_INP()                   \
     {                               \
         M0P_GPIO->PCADS_f.PC06 = 1; \
