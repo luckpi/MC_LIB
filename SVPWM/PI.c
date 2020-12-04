ff220e5023be8a210b24ab3f32d9a315b7a82f76
GuMingYang <1666366054@qq.com>
2020-12-04 16:18:08 +0800

    优化速度环调节速度，避免过冲

--- SVPWM/PI.c
+++ SVPWM/PI.c
@@ -6,7 +6,6 @@
 #include "svgen_dq.h"
 #include "MotorConfig.h"
 
-// float VelRefRaw;
 tCtrlParm CtrlParm;
 tPIParm PIParmQ;    /* Q轴电流PI控制器的参数 */
 tPIParm PIParmD;    /* D轴电流PI控制器的参数 */
@@ -27,28 +26,28 @@ void PI_Parameters(void)
 {
 
     // PI D Term
-    PIParmD.qKp = D_CURRCNTR_PTERM;
-    PIParmD.qKi = D_CURRCNTR_ITERM;
-    PIParmD.qKc = D_CURRCNTR_CTERM;
-    PIParmD.qOutMax = D_CURRCNTR_OUTMAX;
+    PIParmD.qKp = Q15(D_CURRCNTR_PTERM) << 4;
+    PIParmD.qKi = Q15(D_CURRCNTR_ITERM);
+    PIParmD.qKc = Q15(D_CURRCNTR_CTERM);
+    PIParmD.qOutMax = Q15(D_CURRCNTR_OUTMAX);
     PIParmD.qOutMin = -PIParmD.qOutMax;
 
     InitPI(&PIParmD);
 
     // PI Q Term
-    PIParmQ.qKp = Q_CURRCNTR_PTERM;
-    PIParmQ.qKi = Q_CURRCNTR_ITERM;
-    PIParmQ.qKc = Q_CURRCNTR_CTERM;
-    PIParmQ.qOutMax = Q_CURRCNTR_OUTMAX;
+    PIParmQ.qKp = Q15(Q_CURRCNTR_PTERM) << 4;
+    PIParmQ.qKi = Q15(Q_CURRCNTR_ITERM);
+    PIParmQ.qKc = Q15(Q_CURRCNTR_CTERM);
+    PIParmQ.qOutMax = Q15(Q_CURRCNTR_OUTMAX);
     PIParmQ.qOutMin = -PIParmQref.qOutMax;
 
     InitPI(&PIParmQ);
 
     // PI Qref Term
-    PIParmQref.qKp = SPEEDCNTR_PTERM;
-    PIParmQref.qKi = SPEEDCNTR_ITERM;
-    PIParmQref.qKc = SPEEDCNTR_CTERM;
-    PIParmQref.qOutMax = SPEEDCNTR_OUTMAX;
+    PIParmQref.qKp = Q15(SPEEDCNTR_PTERM) << 4;
+    PIParmQref.qKi = Q15(SPEEDCNTR_ITERM);
+    PIParmQref.qKc = Q15(SPEEDCNTR_CTERM);
+    PIParmQref.qOutMax = Q15(SPEEDCNTR_OUTMAX);
     PIParmQref.qOutMin = -PIParmQref.qOutMax;
 
     InitPI(&PIParmQref);
@@ -65,13 +64,13 @@ void PI_Parameters(void)
 *****************************************************************************/
 static void CalcPI(tPIParm *pParm)
 {
-    int32_t Err;
+    int16_t Err;
     int32_t U;
     int32_t Exc;
 
     Err = pParm->qInRef - pParm->qInMeas;
     pParm->qErr = Err;
-    U = pParm->qdSum + _IQmpy((pParm->qKp << 4), Err);
+    U = pParm->qdSum + _IQmpy(pParm->qKp, Err);
 
     if (U > pParm->qOutMax)
     {
@@ -87,7 +86,7 @@ static void CalcPI(tPIParm *pParm)
     }
 
     Exc = U - pParm->qOut;
-    pParm->qdSum += (int16_t)(_IQmpy(pParm->qKi, Err)) - (int16_t)(_IQmpy(pParm->qKc, Exc));
+    pParm->qdSum += _IQmpy(pParm->qKi, Err) - _IQmpy(pParm->qKc, Exc);
 }
 /*****************************************************************************
  函 数 名  : PI_Control
@@ -97,7 +96,8 @@ static void CalcPI(tPIParm *pParm)
 *****************************************************************************/
 void PI_Control(void)
 {
-    volatile int16_t temp1;
+    volatile int16_t temp1, temp2, VelRefRaw;
+
     if (mcState == mcAlign || mcState == mcDrag) // 开环强拖
     {
         // q当前参考等于vel参考
@@ -120,30 +120,46 @@ void PI_Control(void)
     }
     else if (mcState == mcRun) // 闭环
     {
-
-        // VelRefRaw = (float)(ADCSample.POT * POT_ADC_COUNT_FW_SPEED_RATIO); //速度控制，值瞎给的
-        // /* LPF */
-        // CtrlParm.VelRef = (RL_1MINUS_WCTS_VELREF * (CtrlParm.VelRef)) + (RL_WCTS_VELREF * (VelRefRaw));
-        // CtrlParm.VelRef = (ADCSample.POT * POT_ADC_COUNT_FW_SPEED_RATIO) << 11;
-        if (CtrlParm.VelRef < MotorCfg.OpenLoopSpeedEnd)
+        if (AccumThetaCnt == 0)
         {
-            CtrlParm.VelRef = MotorCfg.OpenLoopSpeedEnd;
+            VelRefRaw = ((ADCSample.POT - 1900) * MotorCfg.OmegaMax) >> 11; //速度控制，值瞎给的
+            // VelRefRaw = MotorCfg.OmegaMin;
+            // 执行速度控制循环
+            if (VelRefRaw < MotorCfg.OmegaMin)
+            {
+                VelRefRaw = MotorCfg.OmegaMin;
+            }
+            else if (VelRefRaw > MotorCfg.OmegaMax)
+            {
+                VelRefRaw = MotorCfg.OmegaMax;
+            }
+            temp2 = CtrlParm.VelRef - VelRefRaw;
+            if (temp2 < 0)
+            {
+                CtrlParm.VelRef += SPEEDREFRAMP;
+            }
+            else if (temp2 > 0)
+            {
+                if (temp2 > SPEEDREFRAMP)
+                {
+                    CtrlParm.VelRef -= SPEEDREFRAMP;
+                }
+                else
+                {
+                    CtrlParm.VelRef = VelRefRaw;
+                }
+            }
+            PIParmQref.qInMeas = smc.Omega;                          // 反馈速度
+            PIParmQref.qInRef = CtrlParm.VelRef * HoldParm.RotorDirection; // 电机参考速度和方向
+            CalcPI(&PIParmQref);
+            CtrlParm.IqRef = PIParmQref.qOut;
         }
-
-        // 执行速度控制循环
-        PIParmQref.qInMeas = smc.OmegaFltred;                          // 反馈速度
-        PIParmQref.qInRef = CtrlParm.VelRef * HoldParm.RotorDirection; // 电机参考速度和方向
-        CalcPI(&PIParmQref);
-        CtrlParm.IqRef = PIParmQref.qOut;
-
         CtrlParm.IdRef = 0;
-
         // PI control for D
         PIParmD.qInMeas = SVM.Id;
         PIParmD.qInRef = CtrlParm.IdRef;
         CalcPI(&PIParmD);
         SVM.Vd = PIParmD.qOut; // 这是%，如果应转换为V，则乘以 (DC / 2)
-
         /* 具有d分量优先级的动态d-q调整*/
         // 向量限制
         // Vd is 不限
