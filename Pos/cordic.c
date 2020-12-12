35e1e001903d21c17610ce822ae186a1b5639fc1
GuMingYang <1666366054@qq.com>
2020-12-12 15:21:36 +0800

    去掉for循环优化速度

--- Pos/cordic.c
+++ Pos/cordic.c
@@ -1,7 +1,23 @@
 #include "smc.h"
-uint16_t atandiv[] = {8192, 4836, 2555, 1297,
-                      651, 326, 163, 81, 41,
-                      20, 10, 5, 3, 1};
+
+#define ATAN1DIV1       (int16_t)8192
+#define ATAN1DIV2       (int16_t)4836
+#define ATAN1DIV4       (int16_t)2555
+#define ATAN1DIV8       (int16_t)1297
+#define ATAN1DIV16      (int16_t)651
+#define ATAN1DIV32      (int16_t)326
+#define ATAN1DIV64      (int16_t)163
+#define ATAN1DIV128     (int16_t)81
+#define ATAN1DIV256     (int16_t)41
+#define ATAN1DIV512     (int16_t)20
+#define ATAN1DIV1024    (int16_t)10
+#define ATAN1DIV2048    (int16_t)5
+#define ATAN1DIV4096    (int16_t)3
+#define ATAN1DIV8192    (int16_t)1
+
+// uint16_t atandiv[] = {8192, 4836, 2555, 1297,
+//                       651, 326, 163, 81, 41,
+//                       20, 10, 5, 3, 1};
 int16_t CORDIC_Atan(int16_t alfa_est, int16_t beta_est)
 {
     int16_t hAngle;
@@ -31,24 +47,160 @@ int16_t CORDIC_Atan(int16_t alfa_est, int16_t beta_est)
         wXi = alfa_est >> 1;
         wYi = beta_est >> 1;
     }
-    for (uint8_t i = 0; i < 8; i++)
+    wXold = wXi;
+    // for (uint8_t i = 0; i < 8; i++)
+    // {
+    //     wXold = wXi;
+    //     /* 开始逐次逼近过程 */
+    //     if (wYi < 0)
+    //     {
+    //         /* 向量在IV象限中 */
+    //         hAngle += atandiv[i];
+    //         wXi -= wYi >> i;
+    //         wYi += wXold >> i;
+    //     }
+    //     else
+    //     {
+    //         /* 向量在象限I中 */
+    //         hAngle -= atandiv[i];
+    //         wXi += wYi >> i;
+    //         wYi -= wXold >> i;
+    //     }
+    // }
+    /*begin the successive approximation process*/
+    /*iteration0*/
+    if (wYi < 0)
     {
-        wXold = wXi;
-        /* 开始逐次逼近过程 */
-        if (wYi < 0)
-        {
-            /* 向量在IV象限中 */
-            hAngle += atandiv[i];
-            wXi -= wYi >> i;
-            wYi += wXold >> i;
-        }
-        else
-        {
-            /* 向量在象限I中 */
-            hAngle -= atandiv[i];
-            wXi += wYi >> i;
-            wYi -= wXold >> i;
-        }
+        /*vector is in Quadrant IV*/
+        hAngle += ATAN1DIV1;
+        wXi = wXi - wYi;
+        wYi = wXold + wYi;
+    }
+    else
+    {
+        /*vector is in Quadrant I*/
+        hAngle -= ATAN1DIV1;
+        wXi = wXi + wYi;
+        wYi = -wXold + wYi;
+    }
+    wXold = wXi;
+
+    /*iteration1*/
+    if (wYi < 0)
+    {
+        /*vector is in Quadrant IV*/
+        hAngle += ATAN1DIV2;
+        wXi -= wYi >> 1;
+        wYi += wXold >> 1;
+    }
+    else
+    {
+        /*vector is in Quadrant I*/
+        hAngle -= ATAN1DIV2;
+        wXi += wYi >> 1;
+        wYi -= wXold >> 1;
+    }
+    wXold = wXi;
+
+    /*iteration2*/
+    if (wYi < 0)
+    {
+        /*vector is in Quadrant IV*/
+        hAngle += ATAN1DIV4;
+        wXi -= wYi >> 2;
+        wYi += wXold >> 2;
+    }
+    else
+    {
+        /*vector is in Quadrant I*/
+        hAngle -= ATAN1DIV4;
+        wXi += wYi >> 2;
+        wYi -= wXold >> 2;
+    }
+    wXold = wXi;
+
+    /*iteration3*/
+    if (wYi < 0)
+    {
+        /*vector is in Quadrant IV*/
+        hAngle += ATAN1DIV8;
+        wXi -= wYi >> 3;
+        wYi += wXold >> 3;
+    }
+    else
+    {
+        /*vector is in Quadrant I*/
+        hAngle -= ATAN1DIV8;
+        wXi += wYi >> 3;
+        wYi -= wXold >> 3;
+    }
+    wXold = wXi;
+
+    /*iteration4*/
+    if (wYi < 0)
+    {
+        /*vector is in Quadrant IV*/
+        hAngle += ATAN1DIV16;
+        wXi -= wYi >> 4;
+        wYi += wXold >> 4;
+    }
+    else
+    {
+        /*vector is in Quadrant I*/
+        hAngle -= ATAN1DIV16;
+        wXi += wYi >> 4;
+        wYi -= wXold >> 4;
+    }
+    wXold = wXi;
+
+    /*iteration5*/
+    if (wYi < 0)
+    {
+        /*vector is in Quadrant IV*/
+        hAngle += ATAN1DIV32;
+        wXi -= wYi >> 5;
+        wYi += wXold >> 5;
+    }
+    else
+    {
+        /*vector is in Quadrant I*/
+        hAngle -= ATAN1DIV32;
+        wXi += wYi >> 5;
+        wYi -= wXold >> 5;
+    }
+    wXold = wXi;
+
+    /*iteration6*/
+    if (wYi < 0)
+    {
+        /*vector is in Quadrant IV*/
+        hAngle += ATAN1DIV64;
+        wXi -= wYi >> 6;
+        wYi += wXold >> 6;
+    }
+    else
+    {
+        /*vector is in Quadrant I*/
+        hAngle -= ATAN1DIV64;
+        wXi += wYi >> 6;
+        wYi -= wXold >> 6;
+    }
+    wXold = wXi;
+
+    /*iteration7*/
+    if (wYi < 0)
+    {
+        /*vector is in Quadrant IV*/
+        hAngle += ATAN1DIV128;
+        wXi -= wYi >> 7;
+        wYi += wXold >> 7;
+    }
+    else
+    {
+        /*vector is in Quadrant I*/
+        hAngle -= ATAN1DIV128;
+        wXi += wYi >> 7;
+        wYi -= wXold >> 7;
     }
     return (hAngle);
 }
