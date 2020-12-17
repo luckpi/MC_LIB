4ff712d293f019e3cb9589cacf2fb9a03116ade5
GuMingYang <1666366054@qq.com>
2020-12-17 17:48:59 +0800

    电流采样偏执电压加入滑动均值滤波

--- Sensor/halless.c
+++ Sensor/halless.c
@@ -8,7 +8,38 @@
 #include "IQmath.h"
 #include "smc.h"
 #include "PI.h"
+/* 基于滑动均值滤波器的电流偏移计算参数 */
+#define MOVING_AVG_WINDOW_SIZE          18 // 移动平均窗口样本大小 is 2^18
+#define CURRENT_OFFSET_MAX              2200   // 当前偏移最大限制
+#define CURRENT_OFFSET_MIN              1900   // 当前偏移最小限制
+#define CURRENT_OFFSET_INIT             2048  // 由于OPAMP偏置在VDD / 2，
+#define ADC_CURRENT_SCALE               16      // 将采样值转换为Q15格式
+uint32_t cumulative_sum_phaseA = (CURRENT_OFFSET_INIT << MOVING_AVG_WINDOW_SIZE);
+uint32_t cumulative_sum_phaseB = (CURRENT_OFFSET_INIT << MOVING_AVG_WINDOW_SIZE);
+uint32_t moving_average_phaseA = 0;
+uint32_t moving_average_phaseB = 0;
 
+/*****************************************************************************
+ 函 数 名  : Moving_Average_Filter
+ 功能描述  : 滑动均值滤波，用于计算偏移值
+ 输入参数  : 无
+ 输出参数  : void
+*****************************************************************************/
+static void Moving_Average_Filter(uint32_t *sum, uint32_t *avg, int16_t *i)
+{
+    (*sum) += (*i) - (*avg);
+    (*avg) = (*sum) >> MOVING_AVG_WINDOW_SIZE;
+
+    /* 界定偏移值 */
+    if ((*avg) > CURRENT_OFFSET_MAX)
+    {
+        (*avg) = CURRENT_OFFSET_MAX;
+    }
+    else if ((*avg) < CURRENT_OFFSET_MIN)
+    {
+        *avg = CURRENT_OFFSET_MIN;
+    }
+}
 /*****************************************************************************
  函 数 名  : PhaseCurrentSample
  功能描述  : 采集Ia,Ib相电流
@@ -26,8 +57,15 @@ static void PhaseCurrentSample(void)
     else
     {
         //把电流转变成%比格式 2048转32768 //最大力矩 = 参考电压/(采样电阻*ADC放大倍数)
-        SVM.Ia = -((uint16_t)(*(BaseJqrResultAddress)) - SVM.Ia_C) * 16;
-        SVM.Ib = -((uint16_t)(*(BaseJqrResultAddress + 1)) - SVM.Ib_C) * 16;
+        SVM.Ia = (uint16_t)(*(BaseJqrResultAddress));     // Phase Current A
+        SVM.Ib = (uint16_t)(*(BaseJqrResultAddress + 1)); // Phase Current B
+        /* 实现了滑动均值滤波器以计算当前偏移量。 滑动均值滤波的窗口大小= 2 ^ MOVING_AVG_WINDOW_SIZE */
+        Moving_Average_Filter(&cumulative_sum_phaseA, &moving_average_phaseA, &SVM.Ia);
+        Moving_Average_Filter(&cumulative_sum_phaseB, &moving_average_phaseB, &SVM.Ib);
+        SVM.Ia = moving_average_phaseA - SVM.Ia; // 减去偏移
+        SVM.Ib = moving_average_phaseB - SVM.Ib;
+        SVM.Ia *= ADC_CURRENT_SCALE; // 转Q15
+        SVM.Ib *= ADC_CURRENT_SCALE;
     }
 }
 /*****************************************************************************
@@ -44,6 +82,8 @@ static void ADC_Calibrate(void)
         count = 0;
         SVM.Ia_C >>= 6;
         SVM.Ib_C >>= 6;
+        cumulative_sum_phaseA = SVM.Ia_C << MOVING_AVG_WINDOW_SIZE;
+        cumulative_sum_phaseB = SVM.Ib_C << MOVING_AVG_WINDOW_SIZE;
         mcState = mcInit;
     }
     else
