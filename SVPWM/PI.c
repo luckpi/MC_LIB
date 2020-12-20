e62f3d07030b4c2db9ade9869213d2d0683d9502
GuMingYang <1666366054@qq.com>
2020-12-20 21:35:11 +0800

    更新电位器调速计算

--- SVPWM/PI.c
+++ SVPWM/PI.c
@@ -121,7 +121,7 @@ void PI_Control(void)
     {
         if (AccumThetaCnt == 0)
         {
-            VelRefRaw = (((ADCSample.POT - 1968) * CtrlParm.OmegaMax) >> 11); //速度控制，值瞎给的
+            VelRefRaw = (((ADCSample.POT - 2000) * (CtrlParm.OmegaMax - CtrlParm.OmegaMin)) >> 11) + CtrlParm.OmegaMin; //速度控制，值瞎给的
             // VelRefRaw = CtrlParm.OmegaMin;
             // 执行速度控制循环
             if (VelRefRaw < CtrlParm.OmegaMin)
@@ -148,7 +148,7 @@ void PI_Control(void)
                     CtrlParm.VelRef = VelRefRaw;
                 }
             }
-            PIParmQref.qInMeas = smc.OmegaFltred; // 反馈速度
+            PIParmQref.qInMeas = smc.OmegaFltred;                          // 反馈速度
             PIParmQref.qInRef = CtrlParm.VelRef * HoldParm.RotorDirection; // 电机参考速度和方向
             CalcPI(&PIParmQref);
             CtrlParm.IqRef = PIParmQref.qOut;
